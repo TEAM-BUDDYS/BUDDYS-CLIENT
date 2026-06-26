fc78be085a98d696607877fe38144829eab310c7
seojin15 <leogin3315@gmail.com>
2026-06-27 02:22:09 +0900

    feat: 옵션 추가

--- src/shared/api/api-client.ts
+++ src/shared/api/api-client.ts
@@ -4,6 +4,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const apiClient = ky.create({
   prefix: API_BASE_URL,
+  credentials: 'include',
   timeout: 10000,
   retry: 0,
 });
