cb32fc4be7b559e0ce988b343bfcdec4bf7e1fa7
JS MacBook Pro <j_seokpark@naver.com>
2026-07-01 15:48:00 +0900

    feat: 공통 Textarea 컴포넌트 구현

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,2 +1,4 @@
+export type { TextAreaProps, TextAreaStatus } from './text-area/text-area';
+export { TextArea } from './text-area/text-area';
 export type { TextFieldProps, TextFieldStatus } from './text-field/text-field';
 export { TextField } from './text-field/text-field';
