a06c09d48b84771b720ac8a3e675aed1dbb03e90
winchoose <tmdxor741@naver.com>
2026-07-01 18:59:20 +0900

    fix: 훅 추가 및 absolute 설정

--- src/shared/components/ui/dropdown/dropdown.tsx
+++ src/shared/components/ui/dropdown/dropdown.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import { cn } from '@/lib/cn';
 import { ChevronDownIcon, ChevronUpIcon } from '@/shared/components/icons';
+import { useClickOutside } from '@/shared/hooks/use-click-outside';
 
 import { OptionItem } from './option-item';
 
@@ -23,15 +24,16 @@ export const Dropdown = ({
   placeholder = '선택해주세요.',
 }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useClickOutside<HTMLElement>(() => setIsOpen(false));
   const hasSelectedValue = Boolean(value);
 
   return (
-    <article className="flex h-76 flex-col gap-2">
+    <article ref={dropdownRef} className="relative">
       <button
         disabled={disabled}
         type="button"
         className={cn(
-          'text-body-m-15 flex justify-between rounded-xl bg-gray-50 px-4 py-3.5 text-gray-500',
+          'text-body-m-15 flex w-full justify-between rounded-xl bg-gray-50 px-4 py-3.5 text-gray-500',
           isOpen && 'bg-gray-100',
           hasSelectedValue && 'text-gray-800',
           disabled && 'bg-gray-50 text-gray-200',
@@ -46,7 +48,7 @@ export const Dropdown = ({
         )}
       </button>
       {isOpen && !disabled && (
-        <ul className="max-h-59 overflow-y-auto rounded-xl border border-gray-200 px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.25)] [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:border-4 [&::-webkit-scrollbar-thumb]:border-transparent [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-thumb]:bg-clip-content [&::-webkit-scrollbar-track]:bg-transparent">
+        <ul className="absolute top-full z-10 mt-2 max-h-59 w-full overflow-y-auto rounded-xl border border-gray-200 bg-white px-4 py-2 shadow-[0_2px_2px_0_rgba(0,0,0,0.25)] [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:border-4 [&::-webkit-scrollbar-thumb]:border-transparent [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-thumb]:bg-clip-content [&::-webkit-scrollbar-track]:bg-transparent">
           {options.map((option) => (
             <OptionItem
               key={option}
