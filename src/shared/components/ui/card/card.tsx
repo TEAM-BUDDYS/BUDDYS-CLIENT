0c9ab60aac47020af6decc6181d4fa6e100dc77f
winchoose <tmdxor741@naver.com>
2026-07-02 22:22:40 +0900

    feat: card 컴포넌트 구현

---
+++ src/shared/components/ui/card/card.tsx
@@ -0,0 +1,45 @@
+import { CardDate } from '@/shared/components/ui/card/card-date';
+import { PostStatusTag, Tag } from '@/shared/components/ui/card/card-tag';
+
+import type { RecruitmentStatus } from './card-tag';
+
+interface CardProps {
+  title: string;
+  content: string;
+  postStatus: RecruitmentStatus;
+  tagValue: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+}
+
+export const Card = ({
+  title,
+  content,
+  postStatus,
+  tagValue,
+  startDate,
+  endDate,
+  image,
+}: CardProps) => {
+  return (
+    <article className="flex justify-between">
+      <section className="flex flex-col gap-2">
+        <span className="flex gap-1.5">
+          <PostStatusTag status={postStatus} />
+          <Tag value={tagValue} />
+        </span>
+        <div className="flex w-45.5 flex-col gap-1">
+          <span className="text-body-sb-16 truncate text-gray-800">
+            {title}
+          </span>
+          <span className="text-body-r-14 truncate text-gray-500">
+            {content}
+          </span>
+        </div>
+        <CardDate startDate={startDate} endDate={endDate} />
+      </section>
+      <img src={image} alt={`${title} 썸네일`} className="size-27 rounded-xl" />
+    </article>
+  );
+};
