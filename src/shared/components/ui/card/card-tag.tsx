610ae5f7d270fa82e679b8698bfe87dd27dd0df1
winchoose <tmdxor741@naver.com>
2026-07-02 22:48:38 +0900

    feat: 디자인 등록 후 수정

--- src/shared/components/ui/card/card-tag.tsx
+++ src/shared/components/ui/card/card-tag.tsx
@@ -16,7 +16,7 @@ export const PostStatusTag = ({ status }: PostStatusTagProps) => {
     <span
       className={cn(
         'text-caption-m-12 rounded px-2 py-0.5',
-        status === 'RECRUITING' && 'bg-[#ff968d] text-white',
+        status === 'RECRUITING' && 'bg-badge text-white',
         status === 'COMPLETED' && 'bg-gray-50 text-gray-200',
       )}
     >
