63dd9d7a33129151ef446f62d5251f177297b5ba
winchoose <tmdxor741@naver.com>
2026-07-06 01:59:33 +0900

    fix: 코드래빗 리뷰

--- src/domains/home/components/card-profile.tsx
+++ src/domains/home/components/card-profile.tsx
@@ -21,25 +21,27 @@ export const CardProfile = ({
   href,
 }: CardProfileProps) => {
   return (
-    <Link
-      href={href}
-      className="flex h-42 w-35.25 flex-col items-center justify-between gap-2 rounded-2xl border border-gray-200 px-10 py-4"
-    >
-      <Image
-        src={imageUrl}
-        alt={`${nickname} 프로필 이미지`}
-        width={60}
-        height={60}
-        unoptimized
-        className="size-15 rounded-full object-cover"
-      />
-      <div className="flex flex-col items-center">
-        <span className="text-body-sb-15 text-gray-800">{nickname}</span>
-        <span className="text-caption-m-10 text-gray-500">
-          {country} · {ageGroup}
-        </span>
-      </div>
-      <Tag value={`매칭 ${matchingRate}%`} />
-    </Link>
+    <article>
+      <Link
+        href={href}
+        className="flex h-42 w-35.25 flex-col items-center justify-between gap-2 rounded-2xl border border-gray-200 px-10 py-4"
+      >
+        <Image
+          src={imageUrl}
+          alt={`${nickname} 프로필 이미지`}
+          width={60}
+          height={60}
+          unoptimized
+          className="size-15 rounded-full object-cover"
+        />
+        <div className="flex flex-col items-center">
+          <span className="text-body-sb-15 text-gray-800">{nickname}</span>
+          <span className="text-caption-m-10 text-gray-500">
+            {country} · {ageGroup}
+          </span>
+        </div>
+        <Tag value={`매칭 ${matchingRate}%`} />
+      </Link>
+    </article>
   );
 };
