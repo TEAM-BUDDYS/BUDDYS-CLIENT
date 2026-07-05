fc595dd3ab28a089c6f208af0a47ed2a9e26a9ce
winchoose <tmdxor741@naver.com>
2026-07-06 01:27:15 +0900

    feat: summary card 컴포넌트 구현

---
+++ src/domains/home/components/summary-card.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+import { CardDate } from '@/shared/components/ui/card/card-date';
+
+interface CardProps {
+  title: string;
+  content: string;
+  startDate: string;
+  endDate: string;
+  image?: string;
+}
+
+export const Card = ({
+  title,
+  content,
+  startDate,
+  endDate,
+  image,
+}: CardProps) => {
+  return (
+    <article className="flex gap-4">
+      {image && (
+        <Image
+          src={image}
+          alt={`${title} 썸네일`}
+          width={108}
+          height={108}
+          unoptimized
+          className="size-18 rounded-xl object-cover"
+        />
+      )}
+      <section className="flex flex-col gap-2">
+        <div className="flex w-46.25 flex-col gap-1">
+          <header className="text-body-sb-16 truncate text-gray-800">
+            {title}
+          </header>
+          <p className="text-caption-m-12 truncate text-gray-500">{content}</p>
+        </div>
+        <CardDate startDate={startDate} endDate={endDate} />
+      </section>
+    </article>
+  );
+};
