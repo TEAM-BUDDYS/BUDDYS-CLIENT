ef471cfbc2bbaafec8d4df766791b39613c2f217
Jina Kim(김진아) <164317584+jinaaaaaaaaaaaaa@users.noreply.github.com>
2026-07-06 12:40:09 +0900

    [Feat] BDYFE-38 Searchbar 구현  (#30)
    * feat: searchbar 구현
    
    * chore: optionList 공통 컴포넌트로 분리 및 searchbar에 접근성을 위한 속성 추가
    
    * style: option-list 스타일 변경
    
    * chore: 리뷰 반영
    
    * chore: 아이콘 크기 지정 방식 수정
    
    * chore: 옵션 선택 시 아이콘 변경 및 선택값 관리 방식 수정
    
    ---------
    
    Co-authored-by: Jina <jinatye@naver.com>

---
+++ src/shared/components/ui/searchbar/searchbar.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { ComponentPropsWithoutRef } from 'react';
+
+import { cn } from '@/lib/cn';
+
+import { SearchIcon, XCircleIcon } from '../../icons';
+
+export type SearchbarSize = 'small' | 'medium';
+
+interface SearchbarProps extends Omit<
+  ComponentPropsWithoutRef<'input'>,
+  'size' | 'value' | 'onChange'
+> {
+  size: SearchbarSize;
+  value: string;
+  isCompleted?: boolean;
+  onChange: (value: string) => void;
+}
+
+const searchbarStyle = {
+  small: 'h-11 w-76.75',
+  medium: 'h-13 w-85.75',
+} as const;
+
+export const Searchbar = ({
+  size,
+  value,
+  isCompleted = false,
+  onChange,
+  placeholder = '검색어를 입력해주세요',
+  ...inputProps
+}: SearchbarProps) => {
+  const { className: inputClassName, ...restInputProps } = inputProps ?? {};
+
+  const showClearButton = value.length > 0 && !isCompleted;
+
+  return (
+    <div
+      className={`flex ${searchbarStyle[size]} items-center justify-between rounded-xl bg-gray-50 py-2.5 pr-3 pl-4`}
+    >
+      <input
+        {...restInputProps}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className={cn(
+          'text-body-m-15 flex-1 bg-transparent text-gray-800 outline-none placeholder:text-gray-500',
+          inputClassName,
+        )}
+      />
+      {showClearButton ? (
+        <button
+          type="button"
+          onClick={() => onChange('')}
+          aria-label="검색어 삭제"
+        >
+          <XCircleIcon className="size-6 shrink-0 text-gray-200" />
+        </button>
+      ) : (
+        <SearchIcon className="size-6 shrink-0 text-gray-500" />
+      )}
+    </div>
+  );
+};
