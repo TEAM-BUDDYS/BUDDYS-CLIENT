d26c7744d11ea7942ef7259f65ee6a579541717d
JS MacBook Pro <j_seokpark@naver.com>
2026-07-07 22:29:19 +0900

    chore: 홈 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { CommentItem } from '@/domains/posts/components/comment-item/comment-item';
-
 export default function Home() {
-  return (
-    <main className="min-h-dvh bg-white px-4 py-8">
-      <div className="mx-auto w-full max-w-[343px]">
-        <CommentItem
-          content="저 관심 있어요!"
-          author={{
-            userId: 10,
-            nickname: '가윤',
-            profileImageUrl: 'https://picsum.photos/id/64/80/80',
-          }}
-          createdAt="1시간 전"
-        />
-      </div>
-    </main>
-  );
+  return <>Buddys Web</>;
 }
