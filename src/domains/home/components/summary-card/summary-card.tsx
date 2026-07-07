843e40806f736cfb2a9104ee8f8c16efa46f507a
winchoose <tmdxor741@naver.com>
2026-07-07 22:47:24 +0900

    fix: 디자인 수정

--- src/domains/home/components/summary-card/summary-card.tsx
+++ src/domains/home/components/summary-card/summary-card.tsx
@@ -18,7 +18,16 @@ export const SummaryCard = ({
   image,
 }: SummaryCardProps) => {
   return (
-    <article className="flex gap-4">
+    <article className="flex justify-between">
+      <section className="flex flex-col gap-2">
+        <div className="flex w-57.5 flex-col gap-1">
+          <header className="text-body-sb-16 truncate text-gray-800">
+            {title}
+          </header>
+          <p className="text-caption-m-12 truncate text-gray-500">{content}</p>
+        </div>
+        <CardDate startDate={startDate} endDate={endDate} />
+      </section>
       {image && (
         <Image
           src={image}
@@ -29,15 +38,6 @@ export const SummaryCard = ({
           className="size-18 rounded-xl object-cover"
         />
       )}
-      <section className="flex flex-col gap-2">
-        <div className="flex w-46.25 flex-col gap-1">
-          <header className="text-body-sb-16 truncate text-gray-800">
-            {title}
-          </header>
-          <p className="text-caption-m-12 truncate text-gray-500">{content}</p>
-        </div>
-        <CardDate startDate={startDate} endDate={endDate} />
-      </section>
     </article>
   );
 };
