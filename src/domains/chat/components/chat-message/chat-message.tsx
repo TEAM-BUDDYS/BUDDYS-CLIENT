b46127f1836b886f88c8b2342020da89a9fb85d3
Jina <jinatye@naver.com>
2026-07-08 00:00:29 +0900

    feat: chatMessage 컴포넌트 구조 수정 및 type으로 incoming/outcoming 렌더링

--- src/domains/chat/components/chat-message/chat-message.tsx
+++ src/domains/chat/components/chat-message/chat-message.tsx
@@ -1,6 +1,8 @@
 import { cn } from '@/lib/cn';
+import { CommonImage } from '@/shared/components/ui';
 
 import { formatChatTime } from '../../utils/format-chat-time';
+import profileImg from './달뭉.webp';
 
 interface ChatMessageProps {
   type: 'incoming' | 'outgoing';
@@ -21,29 +23,44 @@ export const ChatMessage = ({
 
   return (
     <div
-      className={cn(
-        'flex flex-col gap-2',
-        isOutgoing ? 'items-end' : 'items-start',
-      )}
+      className={cn('flex', isOutgoing ? 'justify-end' : 'justify-start gap-4')}
     >
-      <div className="flex items-end gap-2">
-        {isRead && (
-          <span className="text-caption-r-12 text-gray-500">읽음</span>
+      {!isOutgoing && (
+        <CommonImage
+          radius="rounded-full"
+          src={profileImg}
+          alt="프로필 이미지"
+          width={40}
+          height={40}
+          className="size-10"
+        />
+      )}
+
+      <div
+        className={cn(
+          'flex flex-col gap-2',
+          isOutgoing ? 'items-end' : 'items-start',
         )}
-        <div
-          className={cn(
-            'text-body-r-14 flex items-center justify-center rounded-lg p-3',
-            isOutgoing
-              ? 'rounded-tr-xs bg-gray-800 text-white'
-              : 'rounded-tl-xs bg-gray-50',
+      >
+        <div className="flex items-end gap-2">
+          {isOutgoing && isRead && (
+            <span className="text-caption-r-12 text-gray-500">읽음</span>
           )}
-        >
-          {content}
+          <div
+            className={cn(
+              'text-body-r-14 max-w-55 rounded-lg p-3',
+              isOutgoing
+                ? 'rounded-tr-xs bg-gray-800 text-white'
+                : 'rounded-tl-xs bg-gray-50',
+            )}
+          >
+            {content}
+          </div>
         </div>
+        <span className="text-caption-m-12 text-gray-500">
+          {formatChatTime(createdAt)}
+        </span>
       </div>
-      <span className="text-caption-m-12 text-end text-gray-500">
-        {formatChatTime(createdAt)}
-      </span>
     </div>
   );
 };
