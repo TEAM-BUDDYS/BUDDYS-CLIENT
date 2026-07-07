4ac1e06514b47f3f317a676ee5232227fdd0c8df
winchoose <tmdxor741@naver.com>
2026-07-07 23:10:58 +0900

    [Feat] BDYFE-47 홈 Summary Card 컴포넌트 구현 (#39)
    * feat: summary card 컴포넌트 구현
    
    * fix: 오타 수정
    
    * fix: 디자인 수정
    
    * fix: 이미지 컴포넌트 교체

---
+++ src/domains/home/components/summary-card/summary-card.tsx
@@ -0,0 +1,42 @@
+import { CardDate } from '@/shared/components/ui/card/card-date';
+import { CommonImage } from '@/shared/components/ui/common-image/common-image';
+
+interface SummaryCardProps {
+  title: string;
+  content: string;
+  startDate: string;
+  endDate: string;
+  image?: string;
+}
+
+export const SummaryCard = ({
+  title,
+  content,
+  startDate,
+  endDate,
+  image,
+}: SummaryCardProps) => {
+  return (
+    <article className="flex justify-between">
+      <section className="flex flex-col gap-2">
+        <div className="flex w-57.5 flex-col gap-1">
+          <header className="text-body-sb-16 truncate text-gray-800">
+            {title}
+          </header>
+          <p className="text-caption-m-12 truncate text-gray-500">{content}</p>
+        </div>
+        <CardDate startDate={startDate} endDate={endDate} />
+      </section>
+      {image && (
+        <CommonImage
+          src={image}
+          alt={`${title} 썸네일`}
+          width={72}
+          height={72}
+          unoptimized
+          radius="rounded-xl"
+        />
+      )}
+    </article>
+  );
+};
