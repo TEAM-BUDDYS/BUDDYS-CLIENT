548e407b73bb253a421d8247703fd2887dfa3d59
ahyohyo <sywjsx0x@gmail.com>
2026-07-08 02:32:46 +0900

    refactor: 코드래빗 리뷰반영

--- src/domains/profile/components/settings-menu-item/settings-menu-item.tsx
+++ src/domains/profile/components/settings-menu-item/settings-menu-item.tsx
@@ -7,16 +7,14 @@ interface SettingsMenuItemProps {
   href: string;
 }
 
-export default function SettingsMenuItem({
-  label,
-  href,
-}: SettingsMenuItemProps) {
+export function SettingsMenuItem({ label, href }: SettingsMenuItemProps) {
   return (
-    <Link href={href} className="flex w-full border-b border-gray-100">
-      <div className="flex w-full items-center justify-between px-4 py-5.5">
-        <span className="text-body-sb-16 text-gray-800">{label}</span>
-        <ChevronRightIcon className="size-6 text-gray-300" />
-      </div>
+    <Link
+      href={href}
+      className="flex w-full items-center justify-between border-b border-gray-100 px-4 py-5.5"
+    >
+      <span className="text-body-sb-16 text-gray-800">{label}</span>
+      <ChevronRightIcon className="size-6 text-gray-300" />
     </Link>
   );
 }
