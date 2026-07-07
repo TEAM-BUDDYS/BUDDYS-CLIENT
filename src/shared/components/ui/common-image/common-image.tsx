0dc45814e493ebd11abef29dc8d333d0fcf7e9a2
seojin15 <leogin3315@gmail.com>
2026-07-07 15:27:56 +0900

    fix: 리뷰 반영

--- src/shared/components/ui/common-image/common-image.tsx
+++ src/shared/components/ui/common-image/common-image.tsx
@@ -2,24 +2,18 @@ import Image, { type ImageProps } from 'next/image';
 
 import { cn } from '@/lib/cn';
 
-type Radius = 'full' | 'sm' | 'md' | 'lg';
-
-const radiusStyles = {
-  full: 'rounded-full',
-  sm: 'rounded-[4px]',
-  md: 'rounded-[8px]',
-  lg: 'rounded-[12px]',
-};
-
-interface CommonImageProps extends Omit<ImageProps, 'fill'> {
-  radius: Radius;
+interface CommonImageProps extends Omit<
+  ImageProps,
+  'fill' | 'width' | 'height'
+> {
+  size: number;
+  radius: string;
 }
 
 export const CommonImage = ({
   src,
   alt,
-  width,
-  height,
+  size,
   radius,
   className,
   ...imageProps
@@ -29,13 +23,9 @@ export const CommonImage = ({
       {...imageProps}
       src={src}
       alt={alt}
-      width={width}
-      height={height}
-      className={cn(
-        'aspect-square shrink-0 object-cover',
-        radiusStyles[radius],
-        className,
-      )}
+      width={size}
+      height={size}
+      className={cn('aspect-square shrink-0 object-cover', radius, className)}
     />
   );
 };
