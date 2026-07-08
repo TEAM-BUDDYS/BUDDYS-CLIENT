058e0de366ee5c0400d39ee613a0f5ec3d0c032b
JS MacBook Pro <j_seokpark@naver.com>
2026-07-08 15:14:57 +0900

    feat: 전역 provider에 ToastProvider 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { type ReactNode, useState } from 'react';
 
+import { ToastProvider } from '@/shared/components/ui';
+
 interface ProvidersProps {
   children: ReactNode;
 }
@@ -24,7 +26,7 @@ export const Providers = ({ children }: ProvidersProps) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <ToastProvider>{children}</ToastProvider>
       {process.env.NODE_ENV === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
