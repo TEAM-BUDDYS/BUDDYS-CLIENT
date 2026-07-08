47d6b1639bcd5de1a82d1f6279235b0952465191
seojin15 <leogin3315@gmail.com>
2026-07-08 20:27:39 +0900

    fix: 리뷰 반영

--- src/domains/chat/components/chat-list-item/chat-list-item.tsx
+++ src/domains/chat/components/chat-list-item/chat-list-item.tsx
@@ -6,7 +6,7 @@ interface ChatListItemProps {
   nickname: string;
   lastMessage: string;
   sentAt: string;
-  unreadCount?: number;
+  unreadCount: number;
 }
 
 export const ChatListItem = ({
@@ -16,21 +16,25 @@ export const ChatListItem = ({
   sentAt,
   unreadCount,
 }: ChatListItemProps) => {
+  const hasUnread = unreadCount > 0;
+
   return (
-    <div className="relative flex w-full justify-between pt-0.5 pl-0.5">
-      {unreadCount && (
-        <span className="bg-mint-300 text-caption-m-12 absolute top-0 left-0 flex size-4.5 items-center justify-center rounded-full px-[5px] text-white">
-          {unreadCount}
-        </span>
-      )}
+    <li className="flex w-full justify-between pt-0.5 pl-0.5">
       <div className="flex max-w-[270px] items-start gap-[13px]">
-        <CommonImage
-          src={imgUrl}
-          alt={`${nickname} 프로필 사진`}
-          width={50}
-          height={50}
-          radius="rounded-full"
-        />
+        <div className="relative flex">
+          <CommonImage
+            src={imgUrl}
+            alt={`${nickname} 프로필 사진`}
+            width={50}
+            height={50}
+            radius="rounded-full"
+          />
+          {hasUnread && (
+            <span className="bg-mint-300 text-caption-m-12 absolute top-[-2px] left-[-2px] flex size-4.5 items-center justify-center rounded-full px-[5px] text-white">
+              {unreadCount}
+            </span>
+          )}
+        </div>
 
         <div className="flex min-w-0 flex-col gap-1">
           <h2 className="text-body-sb-15 text-gray-800">{nickname}</h2>
@@ -41,6 +45,6 @@ export const ChatListItem = ({
       <span className="text-caption-m-12 shrink-0 text-gray-500">
         {formatRelativeTime(sentAt)}
       </span>
-    </div>
+    </li>
   );
 };
