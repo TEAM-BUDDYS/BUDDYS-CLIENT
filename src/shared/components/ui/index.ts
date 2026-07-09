2174e086db53c16a6336e0eb5c6151e683f30795
JS MacBook Pro <j_seokpark@naver.com>
2026-07-09 14:11:07 +0900

    feat: form-label 컴포넌트 구현

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -13,6 +13,7 @@ export { Dropdown } from './dropdown/dropdown';
 export { OptionItem } from './dropdown/option-item';
 export { OptionList } from './dropdown/option-list';
 export { Filter, type FilterProps } from './filter/filter';
+export { FormLabel, type FormLabelProps } from './form-label/form-label';
 export { ProfileImageInput } from './profile-image-input/profile-image-input';
 export { ProgressBar } from './progress-bar/progress-bar';
 export { Searchbar, type SearchbarSize } from './searchbar/searchbar';
