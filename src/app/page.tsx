05e55c045fd1aabbb79314b74bb78dd4b728f5fc
winchoose <tmdxor741@naver.com>
2026-07-09 15:24:57 +0900

    fix: 페이지 원상태

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { PreferenceBuddySection } from '@/domains/home/sections/preference-buddy-section';
-
 export default function Home() {
-  return (
-    <main className="px-4">
-      <PreferenceBuddySection />
-    </main>
-  );
+  return <>Buddys Web</>;
 }
