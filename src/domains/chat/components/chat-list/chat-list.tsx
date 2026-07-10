fbfe9ebef0ca2158243992d4d3bf5eafdda8cbb8
Jina <jinatye@naver.com>
2026-07-10 21:20:50 +0900

    chore: api 연동 위치 수정

--- src/domains/chat/components/chat-list/chat-list.tsx
+++ src/domains/chat/components/chat-list/chat-list.tsx
@@ -1,37 +1,20 @@
-import defaultProfile from '@/shared/assets/illustrations/default-profile.svg';
+import { defaultProfileImage } from '@/shared/assets/illustrations';
 
-import { ChatRoomList } from '../../model/chat-list';
+import { ChatRoom } from '../../model/chat-list';
 import { ChatListItem } from './chat-list-item/chat-list-item';
 
-const MOCK_CHAT_ROOM_LIST: ChatRoomList = {
-  chatRooms: [
-    {
-      chatRoomId: 1,
-      participant: {
-        userId: 2,
-        nickname: '민지',
-        profileImageUrl: '/images/profile.png',
-      },
-      lastMessage: '내일 몇 시에 만날까요?',
-      lastMessageSentAt: '2026-07-05T14:30:00',
-      unreadMessageCount: 3,
-    },
-  ],
-  page: 0,
-  size: 20,
-  hasNext: true,
-};
+interface ChatListProps {
+  chatRooms: ChatRoom[];
+}
 
-export const ChatList = () => {
-  const { chatRooms } = MOCK_CHAT_ROOM_LIST;
-
+export const ChatList = ({ chatRooms }: ChatListProps) => {
   return (
     <ul className="mt-0.5 mb-10">
       {chatRooms.map((chatRoom) => (
         <ChatListItem
           key={chatRoom.chatRoomId}
           chatRoomId={chatRoom.chatRoomId}
-          imgUrl={chatRoom.participant.profileImageUrl ?? defaultProfile}
+          imgUrl={chatRoom.participant.profileImageUrl ?? defaultProfileImage}
           nickname={chatRoom.participant.nickname}
           lastMessage={chatRoom.lastMessage}
           sentAt={chatRoom.lastMessageSentAt}
