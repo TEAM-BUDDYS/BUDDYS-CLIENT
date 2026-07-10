22b551fa49d01b3f5352b7b298e7b32a09312c25
winchoose <tmdxor741@naver.com>
2026-07-11 02:10:04 +0900

    [Feat]: BDYFE-106 SearchSheet 구현 (#94)
    * feat: 서치 시트 구현
    
    * fix: 돌리드 리뷰 반영
    
    * fix: 돌리드 리뷰 반영
    
    * fix: 돌리드 리뷰 반영

---
+++ src/domains/home/features/search-sheet/search-sheet.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { SearchHistory } from '@/domains/home/components/search-history/search-history';
+import { useSearchSheet } from '@/domains/home/features/search-sheet/use-search-sheet';
+import { Header } from '@/shared/components/layout';
+import { FormLabel, Searchbar } from '@/shared/components/ui';
+
+export const SearchSheet = () => {
+  const {
+    searchKeyword,
+    searchHistoryItems,
+    suggestionItems,
+    searchHistoryTitle,
+    searchHistoryType,
+    handleSearchKeywordChange,
+    handleSearchHistorySelect,
+    handleSearchHistoryDelete,
+  } = useSearchSheet();
+
+  return (
+    <>
+      <Header
+        hasBackButton
+        content={
+          <Searchbar
+            size="small"
+            value={searchKeyword}
+            onChange={handleSearchKeywordChange}
+          />
+        }
+      />
+      <main className="mt-4 flex flex-col px-4">
+        <FormLabel as="p" className="text-body-sb-16">
+          {searchHistoryTitle}
+        </FormLabel>
+        <div className="mt-4">
+          {searchHistoryType === 'history' ? (
+            <SearchHistory
+              type="history"
+              items={searchHistoryItems}
+              onSelect={handleSearchHistorySelect}
+              onDelete={handleSearchHistoryDelete}
+            />
+          ) : (
+            <SearchHistory
+              type="suggestion"
+              items={suggestionItems}
+              onSelect={handleSearchHistorySelect}
+            />
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
