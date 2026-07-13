c4b6c42ab8fab91f565d22ef103653c2b10a7872
ahyohyo <sywjsx0x@gmail.com>
2026-07-14 01:47:05 +0900

    refactor: 프로필 AsyncBoundary를 컨테이너 컴포넌트로 통합

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,5 @@
 import { MyProfileContainer } from '@/domains/profile/components/my-profile-container/my-profile-container';
-import { ProfileAsyncBoundary } from '@/domains/profile/components/profile-async-boundary/profile-async-boundary';
 
 export default function ProfilePage() {
-  return (
-    <ProfileAsyncBoundary>
-      <MyProfileContainer />
-    </ProfileAsyncBoundary>
-  );
+  return <MyProfileContainer />;
 }
