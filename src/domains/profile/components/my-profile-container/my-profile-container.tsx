64e58d93eb10df7f7c81cf81f0835bbde36b75ea
ahyohyo <sywjsx0x@gmail.com>
2026-07-14 18:47:34 +0900

    style: MyProfileContainer 컴포넌트 선언을 화살표 함수로 변경

--- src/domains/profile/components/my-profile-container/my-profile-container.tsx
+++ src/domains/profile/components/my-profile-container/my-profile-container.tsx
@@ -6,8 +6,8 @@ import { PROFILE_QUERY_OPTIONS } from '@/domains/profile/api/query';
 
 import { ProfilePageView } from '../profile-page-view/profile-page-view';
 
-export function MyProfileContainer() {
+export const MyProfileContainer = () => {
   const { data: profile } = useSuspenseQuery(PROFILE_QUERY_OPTIONS.ME());
 
   return <ProfilePageView profile={profile} />;
-}
+};
