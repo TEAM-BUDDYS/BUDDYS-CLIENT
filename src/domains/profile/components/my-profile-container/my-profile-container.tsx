23bd98975ebea2f3189ea8eacf493ac597b46ccb
ahyohyo <sywjsx0x@gmail.com>
2026-07-15 17:02:14 +0900

    [Feat] BDYFE-121 내 프로필 조회 API 연동 (#111)
    * feat: 내 프로필 조회 api 연동
    
    * refactor: 전역 Suspense & ErrorBoundary 처리 적용
    
    * fix: 내 프로필 조회 예외 처리 보강
    
    * fix: 프로필 페이지 RSC 에러 수정
    
    * refactor: 프로필 AsyncBoundary를 컨테이너 컴포넌트로 통합
    
    * fix: 프로필 페이지 빌드 시 프리렌더 실패 수정
    
    * fix: 프로필 조회 실패 시 원본 에러 메시지 노출 방지
    
    * fix: 프로필 페이지 인증 완료 후 조회하도록 AuthEntryGuard 적용
    
    * refactor: MyProfileContainer 조회 결과 변수명을 profile로 명확화
    
    * refactor: 닉네임 검증 함수/타입 네이밍 명확화
    
    * style: MyProfileContainer 컴포넌트 선언을 화살표 함수로 변경

---
+++ src/domains/profile/components/my-profile-container/my-profile-container.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { PROFILE_QUERY_OPTIONS } from '@/domains/profile/api/query';
+
+import { ProfilePageView } from '../profile-page-view/profile-page-view';
+
+export const MyProfileContainer = () => {
+  const { data: profile } = useSuspenseQuery(PROFILE_QUERY_OPTIONS.ME());
+
+  return <ProfilePageView profile={profile} />;
+};
