a600cead7e0e37150cfce443ce0a472101d15998
Jina <jinatye@naver.com>
2026-09-01 16:52:39 +0900

    chore: 이미지 webp로 변환

--- src/domains/onboarding/model/pwa.ts
+++ src/domains/onboarding/model/pwa.ts
@@ -4,12 +4,12 @@ export const PWA_INFO_LIST = [
     description: '브라우저 하단 공유 버튼 탭',
     images: [
       {
-        src: '/images/pwa_step_1_1.png',
+        src: '/images/pwa_step_1_1.webp',
         width: 270,
         height: 54,
       },
       {
-        src: '/images/pwa_step_1_2.png',
+        src: '/images/pwa_step_1_2.webp',
         width: 270,
         height: 54,
       },
@@ -20,7 +20,7 @@ export const PWA_INFO_LIST = [
     description: '홈 화면에 추가 선택',
     images: [
       {
-        src: '/images/pwa_step_2.png',
+        src: '/images/pwa_step_2.webp',
         width: 270,
         height: 73,
       },
@@ -31,7 +31,7 @@ export const PWA_INFO_LIST = [
     description: '추가된 앱 실행',
     images: [
       {
-        src: '/images/pwa_step_3.png',
+        src: '/images/pwa_step_3.webp',
         width: 100,
         height: 94,
       },
