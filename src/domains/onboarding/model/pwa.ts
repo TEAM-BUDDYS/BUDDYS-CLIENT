6f702e2577470c3086ef8cbcdb0d97c996e78f57
Jina <jinatye@naver.com>
2026-09-01 14:55:18 +0900

    feat: pwa_step 구현

---
+++ src/domains/onboarding/model/pwa.ts
@@ -0,0 +1,40 @@
+export const PWA_INFO_LIST = [
+  {
+    step: 1,
+    description: '브라우저 하단 공유 버튼 탭',
+    images: [
+      {
+        src: '/images/pwa_step_1_1.png',
+        width: 270,
+        height: 54,
+      },
+      {
+        src: '/images/pwa_step_1_2.png',
+        width: 270,
+        height: 54,
+      },
+    ],
+  },
+  {
+    step: 2,
+    description: '홈 화면에 추가 선택',
+    images: [
+      {
+        src: '/images/pwa_step_2.png',
+        width: 270,
+        height: 73,
+      },
+    ],
+  },
+  {
+    step: 3,
+    description: '추가된 앱 실행',
+    images: [
+      {
+        src: '/images/pwa_step_3.png',
+        width: 100,
+        height: 94,
+      },
+    ],
+  },
+] as const;
