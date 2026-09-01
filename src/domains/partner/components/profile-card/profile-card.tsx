d60a3a3282d9d1b6a801680397bb2f3afc57c247
seojin15 <leogin3315@gmail.com>
2026-09-01 17:24:40 +0900

    feat: profile card 컴포넌트 구현

---
+++ src/domains/partner/components/profile-card/profile-card.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+
+import { defaultProfileImage } from '@/shared/assets/illustrations';
+import { Tag } from '@/shared/components/ui/card/card-tag';
+import { CommonImage } from '@/shared/components/ui/common-image/common-image';
+import { ROUTES } from '@/shared/config';
+
+interface ProfileCardProps {
+  userId: number;
+  nickname: string;
+  countryName: string;
+  ageRange: string;
+  matchingPercentage: number;
+  profileImageUrl?: string;
+}
+
+export const ProfileCard = ({
+  userId,
+  nickname,
+  countryName,
+  ageRange,
+  matchingPercentage,
+  profileImageUrl,
+}: ProfileCardProps) => {
+  const profileDescription = `${countryName} · ${ageRange}`;
+
+  return (
+    <article>
+      <Link
+        href={ROUTES.PROFILE.DETAIL(userId)}
+        className="flex w-35 flex-col items-center justify-between gap-2 rounded-2xl border border-gray-200 px-10 py-4"
+      >
+        <CommonImage
+          src={profileImageUrl || defaultProfileImage.src}
+          alt={`${nickname} 프로필 이미지`}
+          width={60}
+          height={60}
+          radius="rounded-full"
+          className="size-15 border border-gray-100"
+          onError={(event) => {
+            event.currentTarget.srcset = '';
+            event.currentTarget.src = defaultProfileImage.src;
+          }}
+        />
+        <div className="flex w-full flex-col items-center">
+          <span className="text-body-sb-15 w-30 text-center text-gray-800">
+            {nickname}
+          </span>
+          <span className="text-caption-m-10 text-gray-500">
+            {profileDescription}
+          </span>
+        </div>
+        <Tag value={`매칭 ${matchingPercentage}%`} />
+      </Link>
+    </article>
+  );
+};
