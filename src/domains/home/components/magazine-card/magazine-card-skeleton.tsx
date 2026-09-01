8b98d15ab21b629816ccc7761d4f7d04a8dbbe01
ahyohyo <sywjsx0x@gmail.com>
2026-09-01 10:10:21 +0900

    fix: MagazineCard Skeleton 설명 너비를 Figma 비율(75%)에 맞게 수정

--- src/domains/home/components/magazine-card/magazine-card-skeleton.tsx
+++ src/domains/home/components/magazine-card/magazine-card-skeleton.tsx
@@ -17,7 +17,7 @@ export const MagazineCardSkeleton = ({
 
       <div className="flex w-full flex-col items-start gap-2">
         <Skeleton className="my-0.5 h-5 w-full rounded-[3px]" />
-        <Skeleton className="my-0.5 h-3.5 w-[77.84%] rounded-[3px]" />
+        <Skeleton className="my-0.5 h-3.5 w-3/4 rounded-[3px]" />
       </div>
     </div>
   );
