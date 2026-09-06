4c233a8a4ef316cd6cdda0687f96a4fa84539d21
ahyohyo <sywjsx0x@gmail.com>
2026-09-06 15:59:20 +0900

    fix: 홈 헤더 상하 패딩 피그마 스펙에 맞게 수정

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <>
       <Header
+        className="h-auto items-start pt-5 pb-4"
         content={
           <BuddysLogoIcon
             className="text-gray-800"
