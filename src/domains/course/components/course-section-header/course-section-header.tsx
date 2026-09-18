5d54bfd8793531a7575ee38b0e5d048bc8af333d
winchoose <tmdxor741@naver.com>
2026-09-18 16:36:46 +0900

    feat: course-section-header 구현

--- src/domains/course/components/course-section-header/course-section-header.tsx
+++ src/domains/course/components/course-section-header/course-section-header.tsx
@@ -1,17 +1,25 @@
-import { ComponentProps } from 'react';
+import type { ComponentProps } from 'react';
 
 import { ChevronRightIcon } from '@/shared/components/icons';
 
-type CourseSectionHeaderProps = Pick<ComponentProps<'button'>, 'onClick'>;
+interface CourseSectionHeaderProps extends Pick<
+  ComponentProps<'button'>,
+  'onClick'
+> {
+  title: string;
+}
 
-export const CourseSectionHeader = ({ onClick }: CourseSectionHeaderProps) => {
+export const CourseSectionHeader = ({
+  title,
+  onClick,
+}: CourseSectionHeaderProps) => {
   return (
     <div className="flex justify-between">
-      <span className="text-body-sb-16">원하는 조건의 코스를 찾아보세요</span>
+      <h2 className="text-body-sb-16 text-gray-800">{title}</h2>
       <button
         type="button"
         className="flex items-center"
-        aria-label="맞춤 코스 탐색으로 이동"
+        aria-label={`${title} 더보기`}
         onClick={onClick}
       >
         <ChevronRightIcon className="size-6 text-gray-500" />
