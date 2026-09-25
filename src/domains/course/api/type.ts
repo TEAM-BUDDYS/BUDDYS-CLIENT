d38112e095f2ec30da662dd5b56415521817b9dd
JS MacBook Pro <j_seokpark@naver.com>
2026-09-25 12:20:26 +0900

    feat: 코스 일자 응답 타입 확장

--- src/domains/course/api/type.ts
+++ src/domains/course/api/type.ts
@@ -1,3 +1,5 @@
 import type { components } from '@/types/schema';
 
+export type CourseDay = components['schemas']['DayResponse'];
 export type Place = components['schemas']['PlaceResponse'];
+export type BookmarkedPlace = components['schemas']['BookmarkedPlaceResponse'];
